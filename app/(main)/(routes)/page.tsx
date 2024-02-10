393e722f64dd506316caa50a099d99bfc3b74e8d
Rendy Pratama <rendypratama752@gmail.com>
2024-02-11 00:01:05 +0700

    ADD: theme toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from '@/components/theme-toogle';
 import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/" />
+      <ThemeToggle />
     </div>
   );
 }
