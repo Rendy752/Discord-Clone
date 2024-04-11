80c8967d6d77b30e42a1dc2b3e227a4034a59bc4
Rendy Pratama <rendypratama752@gmail.com>
2024-04-11 16:50:08 +0700

    ADD: channel id page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -26,11 +26,17 @@ export const ServerMember = ({
 
     const icon = roleIconMap[member.role];
 
+    const onRedirect = () => {
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    }
+
     return (
-        <button className={cn(
-            "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-            params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
-        )}>
+        <button 
+            onClick={onRedirect}
+            className={cn(
+                "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+                params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+            )}>
             <UserAvatar 
                 src={member.profile.imageUrl}
                 className="h-8 w-8 md:h-8 md:w-8"
