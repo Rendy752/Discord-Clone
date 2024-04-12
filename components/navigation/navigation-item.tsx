b6c341cd8ad6211b01645cb5aa37ca2612e62119
Rendy Pratama <rendypratama752@gmail.com>
2024-04-12 21:36:34 +0700

    FIX: params null handle

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -27,12 +27,12 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
                 className="group relative flex items-center"
             >
                 <div className={cn("absolute left-0 bg-primary rounded-r-full transition-all w-[4px]", 
-                    params.serverId !== id && "group-hover:h-[20px]",
-                    params.serverId === id ? "h-[36px]" : "h-[8px]"
+                    params?.serverId !== id && "group-hover:h-[20px]",
+                    params?.serverId === id ? "h-[36px]" : "h-[8px]"
                 )}
                 />
                 <div className={cn("relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-                    params.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
+                    params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
                 )}>
                     <Image
                         fill
