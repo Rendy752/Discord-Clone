f3eb1e038abb6152eb6366ea69ef6ed9cc1ed8ed
Rendy Pratama <rendypratama752@gmail.com>
2024-04-14 09:34:07 +0700

    REFORMAT: line clamp server and channel header

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -25,7 +25,7 @@ export const ServerHeader = ({
         <DropdownMenu>
             <DropdownMenuTrigger className="focus:outline-none" asChild>
                 <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
-                    {server.name}
+                    <p className="line-clamp-1 text-start">{server.name}</p>
                     <ChevronDown className="h-5 w-5 ml-auto" />
                 </button>
             </DropdownMenuTrigger>
