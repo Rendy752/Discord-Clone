ec1fd5352d7c4d24f8458d2e325e206befafa900
Rendy Pratama <rendypratama752@gmail.com>
2025-04-29 21:52:19 +0700

    REFINE: serverless consideration socket-provider

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -1,57 +1,57 @@
 "use client";
 
-import { 
-    createContext,
-    useContext,
-    useEffect, 
-    useState 
-} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { io as ClientIO } from "socket.io-client";
 
 type SocketContextType = {
-    socket: any | null;
-    isConnected: boolean;
+  socket: any | null;
+  isConnected: boolean;
 };
 
 const SocketContext = createContext<SocketContextType>({
-    socket: null,
-    isConnected: false
+  socket: null,
+  isConnected: false,
 });
 
 export const useSocket = () => {
-    return useContext(SocketContext)
+  return useContext(SocketContext);
 };
 
-export const SocketProvider = ({ 
-    children 
-}: {children: React.ReactNode}) => {
-    const [socket, setSocket] = useState(null);
-    const [isConnected, setIsConnected] = useState(false);
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState<any>(null);
+  const [isConnected, setIsConnected] = useState(false);
 
-    useEffect(() => {
-        const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
-            path: "/api/socket/io",
-            addTraillingSlash: false,
-        });
-        
-        socketInstance.on("connect", () => {
-            setIsConnected(true);
-        });
+  useEffect(() => {
+    const socketInstance = ClientIO(process.env.NEXT_PUBLIC_SITE_URL!, {
+      path: "/api/socket/io",
+      addTrailingSlash: false,
+    });
 
-        socketInstance.on("disconnect", () => {
-            setIsConnected(false);
-        });
+    socketInstance.on("connect", () => {
+      setIsConnected(true);
+    });
 
-        setSocket(socketInstance);
+    socketInstance.on("disconnect", () => {
+      setIsConnected(false);
+    });
 
-        return () => {
-            socketInstance.disconnect();
-        };
-    }, []);
+    setSocket(socketInstance);
 
-    return (
-        <SocketContext.Provider value={{ socket, isConnected }}>
-            {children}
-        </SocketContext.Provider>
-    );
-}
+    const interval = setInterval(() => {
+      if (socketInstance) {
+        setIsConnected(socketInstance.connected);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, isConnected }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
